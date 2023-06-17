a004c555076d1fdae73e2c858a9cb153bb56cddb
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-06-17 01:10:53 -0700

    xx

---
+++ src/_codux/boards/footer/footer.board.tsx
@@ -0,0 +1,10 @@
+import { createBoard } from '@wixc3/react-board';
+import { Footer } from '../../../components/footer/footer';
+
+export default createBoard({
+    name: 'Footer',
+    Board: () => <Footer />,
+    environmentProps: {
+        canvasWidth: 2114,
+    },
+});
