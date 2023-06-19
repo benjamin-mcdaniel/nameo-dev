e28170ff01306d55e5ac2a532cc0a904668a230a
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-06-19 16:19:16 -0700

    fixed colors and alignment

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import styles from './App.module.scss';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Body } from './components/body/body';
+import Header_module from './components/header/header.module.scss';
+import Classnames from 'classnames';
 
 function App() {
     return (
-        <div className={styles.App}>
+        <div className={Classnames(styles.App, Header_module.center)}>
             <Header />
             <Body />
             <Footer />
