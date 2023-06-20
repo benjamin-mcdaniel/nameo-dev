3e18fb47d113fc4ab94103ac89f7bdfd0c1862dc
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-06-19 17:43:52 -0700

    updated lots

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,36 +1,92 @@
 import styles from './body.module.scss';
 import classNames from 'classnames';
+import React, { useState } from 'react';
+import Header_module from '../header/header.module.scss';
+
+interface ToggleUIProps {
+    apiType: string;
+    onApiTypeChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onSearchClick: () => void;
+}
+
+const ToggleUI: React.FC<ToggleUIProps> = ({ apiType, onApiTypeChange, onSearchClick }) => {
+    return (
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    name="apiType"
+                    value="fuzz"
+                    checked={apiType === 'fuzz'}
+                    onChange={onApiTypeChange}
+                />
+                Fuzz
+            </label>
+            <label>
+                <input
+                    type="radio"
+                    name="apiType"
+                    value="raw"
+                    checked={apiType === 'raw'}
+                    onChange={onApiTypeChange}
+                />
+                Raw
+            </label>
+        </div>
+    );
+};
 
 export interface BodyProps {
     className?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
-export const Body = ({ className }: BodyProps) => {
+export const Body: React.FC<BodyProps> = ({ className }) => {
+    const [apiType, setApiType] = useState<string>('fuzz');
+
+    const handleApiTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setApiType(event.target.value);
+    };
+
+    const handleSearchClick = () => {
+        // Perform the search with the selected api_type
+        console.log('Search clicked. API type:', apiType);
+    };
+
     return (
-        <div style={{ height: '85vh' }} className={classNames(styles.root, className, styles.body)}>
+        <div
+            style={{ height: '85vh', justifyContent: 'start', padding: '0px' }}
+            className={classNames(styles.root, className, styles.body)}
+        >
             <div className={styles.sidebar}>
-                <h5 className={styles.center}>sidebar section</h5>
+                <div className={styles.fiftyl}>
+                    Options
+                    <ToggleUI
+                        apiType={apiType}
+                        onApiTypeChange={handleApiTypeChange}
+                        onSearchClick={handleSearchClick}
+                    />
+                </div>
             </div>
-            <div>
+            <div className={styles['top-pad']}>
                 <div className={styles.center}>
-                    <div className={styles.input}>
-                        <span className={styles.input}>
-                            <input />
+                    <div>
+                        <span>
+                            <input className={styles.input} />
+                        </span>
+                    </div>
+                    <div className={styles.search}>
+                        <span className={styles.search}>
+                            <button onClick={handleSearchClick}>Search</button>
                         </span>
                     </div>
                     <div>
-                        <span className={styles.input}>
-                            <button>Search</button>
-                        </span>
+                        <span className={classNames(styles.center, styles.search)}></span>
                     </div>
                 </div>
                 <div className={styles.center}>
                     <h6>api results go here</h6>
                 </div>
+                <div className={styles.center}>Text</div>
             </div>
         </div>
     );
