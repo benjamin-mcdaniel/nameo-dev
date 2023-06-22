f14a58cc15cd23566764cdaa16820662e078a485
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2023-06-22 00:05:01 -0700

    past some issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import styles from './App.module.scss';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Body } from './components/body/body';
-import Header_module from './components/header/header.module.scss';
-import Classnames from 'classnames';
+import headerModule from './components/header/header.module.scss';
+import classNames from 'classnames';
 
 function App() {
-    return (
-        <div className={Classnames(styles.App, Header_module.center)}>
-            <Header />
-            <Body />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className={classNames(styles.App, headerModule.center)}>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
